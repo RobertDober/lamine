c677bc71d60ff8729a0bf6b4fda5ea8c8044d21d
RobertDober <robert.dober@gmail.com>
2024-08-30 17:53:46 +0200

    setup of lazy.nvim and basic plugins

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lamine
+
+My personal setup for neovim, pure lua, lazy.nvim based
+
+## Author
+
+Copyright © 2024 Robert Dober robert.dober@gmail.com
+
+# LICENSE
+
+GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007 or later. Please refer to [LICENSE](LICENSE) for details.
+<!--SPDX-License-Identifier: AGPL-3.0-or-later-->
---
+++ basic.lua
@@ -0,0 +1,41 @@
+return {
+    'tpope/vim-surround',
+    'cloudhead/neovim-fuzzy',
+    'morhetz/gruvbox',
+    'preservim/nerdtree',
+    'tpope/vim-commentary',
+
+    -- Nice to have
+    -- ------------
+    'godlygeek/tabular',
+    'ludovicchabant/vim-gutentags',
+    -- 'leafo/moonscript-vim',
+    --
+    -- LSP WIP
+    -- -------------------
+    -- 'nvim-treesitter/nvim-treesitter',
+    -- 'neovim/nvim-lspconfig',
+    -- { 'mihyaeru21/nvim-ruby-lsp', requires = 'neovim/nvim-lspconfig' },
+    -- 'autozimu/LanguageClient-neovim',
+
+    -- Colorschemes
+    -- ------------
+    -- 'EdenEast/nightfox.nvim',
+    'endel/vim-github-colorscheme',
+    'lifepillar/vim-solarized8',
+    'folke/tokyonight.nvim',
+    'sainnhe/everforest',
+    'elixir-editors/vim-elixir',
+    'catppuccin/nvim',
+    'EdenEast/nightfox.nvim',
+
+    -- 'flazz/vim-colorschemes'
+    -- 'godlygeek/tabular',
+    -- 'hrsh7th/nvim-compe',
+    -- 'hrsh7th/vim-vsnip',
+    -- 'liuchengxu/graphviz.vim',
+    -- 'mracos/mermaid.vim',
+    -- 'neovim/nvim-lspconfig',
+    -- 'tpope/vim-fugitive',
+    -- 'zah/nim.vim',
+}
