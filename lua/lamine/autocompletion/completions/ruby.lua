60afc8b2bac3a62213325af83487281179920837
RobertDober <robert.dober@gmail.com>
2024-11-04 22:41:19 +0100

    Road to v0.1.0
    - working on keymappings to replace vimine keymappings
    - speced all defined keymappings
    - progress on tmux mappings WIP
    - speced lua ftplugin settings
    - autocompletion WIP
    	- negative col offsets fixed
    	- refactored context and api ✓
    	- keybinding for input mode and autocomplete defined ✓
    	- autocompletions to be speced and implemented WIP
    		- markdown WIP (fn%d for footnotes)
    		- lua WIP (req and local req)
    		- ruby WIP (string interpolation @@@ and case, OK, NOK
    		  and ERR)

--- lua/lamine/autocompletion/completions/ruby.lua
+++ lua/lamine/autocompletion/completions/ruby.lua
@@ -7,6 +7,18 @@ return {
     "^(%s*)(.*)(@@@)",
     C.replace_suffix_and_add_lines{lines={}, suffix="#{}", offset={0, -1}}
   },
-  {"^(%s*)case.*(%s*)$", C.replace_suffix_and_add_lines{lines={}, suffix=""}},
+  {
+    "^(%s*)(%sOK)",
+    C.replace_suffix_and_add_lines{lines={}, suffix=" {ok: true, ", offset={0, 999}}
+  },
+  {
+    "^(%s*)(%sNOK)",
+    C.replace_suffix_and_add_lines{lines={}, suffix=" {ok: false, ", offset={0, 999}}
+  },
+  {
+    "^(%s*)(%sERR)",
+    C.replace_suffix_and_add_lines{lines={}, suffix=" {ok: false, ", offset={0, 999}}
+  },
+  {"^(%s*)case.*(%s*)$", C.replace_suffix_and_add_lines{lines={"  ", "end"}, offset={1, 999}, suffix=""}},
 }
 -- SPDX-License-Identifier: AGPL-3.0-or-later
--- spec/autocompletion/functional/ruby_spec.lua
+++ spec/autocompletion/functional/ruby_spec.lua
@@ -12,11 +12,50 @@ describe('completions found for ruby', function()
   it('does not find anything if there is no match', function()
     assert.is_nil(fc(context('epiufhezf gzgjrzoi')))
   end)
-  it('replaces @@@ with the correct #{}', function()
-    local result = fc(context('some string "is@@@'))
-    assert.are.same({'some string "is#{}'}, result.lines)
-    assert.are.same({0, -1}, result.offset)
+
+  describe('string interpolation', function()
+    it('replaces @@@ with the correct #{}', function()
+      local result = fc(context('some string "is@@@'))
+      assert.are.same({'some string "is#{}'}, result.lines)
+      assert.are.same({0, -1}, result.offset)
+    end)
+
+    it('replaces @@@ inside a string', function()
+      local result = fc(context('in @@@ the middle'))
+      assert.are.same({'in #{} the middle'}, result.lines)
+      assert.are.same({0, -1}, result.offset)
+    end)
   end)
-  
+
+  describe('case', function()
+    it('inserts lines, not changing first', function()
+      local result = fc(context('  case some_function()'))
+      local expected = {
+        '  case some_function()',
+        '    ',
+        '  end'
+      }
+      assert.are.same(expected, result.lines)
+      assert.are.same({1, 999}, result.offset)
+    end)
+  end)
+
+  describe('results', function()
+    it('inserts an ok result', function()
+      local result = fc(context('  OK'))
+      assert.are.same({'  {ok: true, '}, result.lines)
+      assert.are.same({0, 999}, result.offset)
+    end)
+    it('inserts an error result', function()
+      local result = fc(context('  ERR'))
+      assert.are.same({'  {ok: false, '}, result.lines)
+      assert.are.same({0, 999}, result.offset)
+    end)
+    it('inserts a not ok result', function()
+      local result = fc(context('  NOK'))
+      assert.are.same({'  {ok: false, '}, result.lines)
+      assert.are.same({0, 999}, result.offset)
+    end)
+  end)
 end)
 -- SPDX-License-Identifier: AGPL-3.0-or-later
